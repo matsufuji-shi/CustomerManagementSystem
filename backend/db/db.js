c2e5680391eb5fa81f1cdde904829db9803ee885
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-09 16:45:00 +0900

    データベース名変更

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,7 @@ const db = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "rootroot",
-    database: "express_db"
+    database: "customers"
 });
 
 module.exports = db;
