d9bbab889e5226e1f749a1753eaab68236423703
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-10 18:42:34 +0900

    docs:console.log削除

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -20,7 +20,6 @@ router.get('/',  (req, res) => {
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   const sql = "SELECT * FROM customers WHERE id = ?";
-  console.log("aiu");
   db.query(sql, [id], (err, result) => {
     if (err) {
       console.error(err);
