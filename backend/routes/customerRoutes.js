950b4f897a92c7b643cb968b72e3534f029dad2b
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-10 14:02:24 +0900

    docs:API設定

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -1,11 +1,89 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/customerController");
+const db = require("../db/db"); 
 
-// ユーザー一覧を取得する
-router.get("/get/users", userController.getUsers);
 
-// 新しいユーザーを追加する
-router.post("/insert/user", userController.insertUser);
+//タスク一覧を取得 (GET /customers)
+router.get('/',  (req, res) => {
+ 
+  const sql = "SELECT * FROM customers ";
+  db.query(sql,(err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("タスクの取得に失敗しました");
+    }
+    res.json(result);
+  });
+});
+
+//特定のタスクを取得 (GET /customers/:id)
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "SELECT * FROM customers WHERE id = ?";
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("タスクの取得に失敗しました");
+    }
+    if (result.length === 0) {
+      return res.status(404).send("タスクが見つかりません");
+    }
+    res.json(result[0]);
+  });
+});
+
+// 新しいタスクを追加 (POST /customers)
+router.post("/", (req, res) => {
+  const { name, email, phone, address, company_name } = req.body;
+  
+  if (!name || !email || !phone || !address) {
+    return res.status(400).send("名前・メールアドレス・電話番号・住所の入力が必要です");
+  }
+
+  const sql = `
+    INSERT INTO customers (name, email, phone, address, company_name)
+    VALUES (?, ?, ?, ?, ?)
+  `;
+  db.query(sql, [name, email, phone, address, company_name], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("タスクの追加に失敗しました");
+    }
+    res.status(201).send("タスクを追加しました");
+  });
+});
+
+// 特定のタスクを更新 (PUT /customers/:id)
+router.put("/:id", (req, res) => {
+  const { name, email, phone, address, company_name } = req.body; 
+  const { id } = req.params;
+
+  if (!name || !email || !phone || !address) {
+    return res.status(400).send("名前・メールアドレス・電話番号・住所の入力が必要です");
+  }
+
+  const sql = "UPDATE tasks SET name = ?, email = ?, phone = ?, address = ?, company_name = ? WHERE id = ?";
+  db.query(sql, [name, email, phone, address, company_name, id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("タスクの更新に失敗しました");
+    }
+    res.send("タスクを更新しました");
+  });
+});
+
+//特定のタスクを削除 (DELETE /customers/:id)
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "DELETE FROM customers WHERE id = ?";
+  
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("タスクの削除に失敗しました");
+    }
+    res.send("タスクを削除しました");
+  });
+});
 
 module.exports = router;
